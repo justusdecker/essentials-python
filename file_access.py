a9cca431060ff71d68525367a1ab8788042ca06c
Justus <justus.d2025@gmail.com>
2025-06-25 21:35:44 +0200

    added json & changed return type issue

--- file_access.py
+++ file_access.py
@@ -6,18 +6,40 @@ def file_read(filepath : str) -> str:
     with open(filepath, 'r') as f:
         return f.read()
     
-def file_write(filepath : str, data : str) -> str:
+def file_write(filepath : str, data : str):
     """
     Opens a file, writes the given data into it.
     Creates a new file if not exist.
     """
     with open(filepath, 'w') as f:
-        return f.write(data)
+        f.write(data)
     
-def file_create(filepath : str, data : str) -> str:
+def file_create(filepath : str, data : str):
     """
     Opens a file, writes the given data into it.
     Raises `FileExistsError` if file exist.
     """
     with open(filepath, 'x') as f:
-        return f.write(data)
+        f.write(data)
+
+
+
+# Standalone
+import json
+json.loads('')
+def json_read(filepath : str) -> dict | list:
+    """
+    Opens a json file, read & return the content as dict or list
+    Raises `FileNotFoundError` if file not exist.
+    Raises `JSONDecodeError` if content is corrupted
+    """
+    with open(filepath, 'r') as f:
+        return json.loads(f)
+    
+def json_write(filepath : str, data : dict | list):
+    """
+    Opens a json file, writes the given data into it.
+    Creates a new json file if not exist.
+    """
+    with open(filepath, 'w') as f:
+        f.write(json.dumps(data))
